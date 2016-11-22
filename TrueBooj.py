8642baafc2b599a1822f519c8aa908cceb1d187d
Andrew Fulton <andrewfulton9@gmail.com>
2016-11-21 21:04:21 -0700

    completed coding assessment

--- TrueBooj.py
+++ TrueBooj.py
@@ -1,2 +1,14 @@
 def truebooj(number):
-    # do code here
+    '''
+    function to return a number, or a string depending on if the remainder of
+    the number argument divided by 3, 5, or 10 is = to 0
+    '''
+
+    if number % 10 == 0:
+        return 'TrueBooj'
+    elif number % 5 == 0:
+        return 'Booj'
+    elif number % 3 == 0:
+        return 'True'
+    else:
+        return str(number)
