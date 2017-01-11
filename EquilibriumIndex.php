62f4af0d9f139267b7b41249cec2909647bbf983
krysler <krysler@rtrt.me>
2017-01-10 19:56:11 -0430

    Created feature branch

--- EquilibriumIndex.php
+++ EquilibriumIndex.php
@@ -4,6 +4,15 @@
 function getEquilibriums($arr) {
 	$output = array();
 	# Logic goes here!
+	if (is_array($arr)){
+		$length 	= count($arr);
+		$left_acum 	= 0;
+		$right_acum = 0;
+
+		for ($index=0; $index < $length; $index++) { 
+			
+		}
+	}
 	return $output;
 }
  
