74b80bd2a65cf44846daef9b2ad157a5d3d43617
youngmoney87 <ctmayers@hotmail.com>
2017-04-06 17:51:56 -0600

    Update EquilibriumIndex.py

--- EquilibriumIndex.py
+++ EquilibriumIndex.py
@@ -1,2 +1,11 @@
 def eqindex(data):
-    #do code here
+    indices = []
+    left, right = 0, sum(data)   # initiate left and right sums
+    for index, current in enumerate(data):
+        right -= current    # keep running total of right sum
+        if right == left:
+            indices.append(index)
+        left += current # keep running total of left sum
+    if not indices:
+        return None
+    return indices
