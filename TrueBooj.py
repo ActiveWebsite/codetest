38c66ee70c71c488676a995f3500d18d1805f797
youngmoney87 <ctmayers@hotmail.com>
2017-04-07 17:52:33 -0600

    Update TrueBooj.py

--- TrueBooj.py
+++ TrueBooj.py
@@ -1,2 +1,14 @@
 def truebooj(number):
-    # do code here
+    for num in range(1, number):
+    output = []
+    if num % 3  == 0:
+        output.append('True')
+    if num % 5 == 0:
+        output.append('Booj')
+    if num % 10 == 0:
+        output.append('TrueBooj')
+
+    if output:
+        print(' '.join(output))
+    else:
+        print(num)
