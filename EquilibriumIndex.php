94a39701cb8973f0be44041612d7252879d40369
Jeff Sheffel <jeff@sheffel.org>
2017-06-02 16:47:36 -0600

    initial solution to equilibrium index problem

--- EquilibriumIndex.php
+++ EquilibriumIndex.php
@@ -2,10 +2,44 @@
 
 namespace Booj\CodeTest;
 
-# Put a valid docblock here!
-function getEquilibriums($arr) {
-    $output = array();
-    # Logic goes here!
-    return $output;
+/**
+ * This function ...
+ *
+ * @param $input array to
+ * @return array
+ */
+function getEquilibriums(array $input) {
+    $solution = [];
+    $index = 0;
+    $sumLeft = 0;
+    $sumRight = array_sum($input);
+    $valuePrevious = 0;
+
+    foreach (array_values($input) as $value) {
+        $sumLeft += $valuePrevious;
+        $sumRight -= $value;
+        if ($sumLeft === $sumRight) {
+            $solution[] = $index; // add equilibrium index to solution
+        }
+        $valuePrevious = $value;
+        $index++;
+    }
+
+    return $solution;
 }
  
+/*
+ * The Booj Code Test as solved by Jeff Sheffel (github.com/jscodefix, jeff@sheffel.org)
+ *
+ * My first thought was to create an elegant and complete solution to this simple code challenge.
+ * Some considerations are input validation, performance, and edge cases.
+ * The edge cases for this problem include consideration for the processing the beginning and the end
+ * of the input array.  It soon becomes apparent that the given definition of "an equilibrium index" does
+ * not consider the array edge case indicies; that is, should the sum of the non-existent elements that
+ * are to the left of index 0 be considered as summing to zero?  In the spirit of PHP, since the non-existent
+ * array values can be considered as null, and adding a null values is equivalent to adding a zero integer,
+ * we can include the first and last indicies to our solution.
+ *     null [0, 1, 2, 3, ... N-1, N] null
+ *
+ * Typically for array problems, coding the array indexing correctly can be tricky.
+ */
