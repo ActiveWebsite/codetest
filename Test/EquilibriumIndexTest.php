c372ebec0ef6d6d3da9bc0001ce908b5fa3d3c4a
Jeff Sheffel <jeff@sheffel.org>
2017-06-02 18:20:56 -0600

    add more phpunit tests... you know, TDD

--- Test/EquilibriumIndexTest.php
+++ Test/EquilibriumIndexTest.php
@@ -1,17 +1,47 @@
 <?php
 
-//namespace Booj\CodeTest\Tests;
+namespace Booj\CodeTest;
 
 require_once("EquilibriumIndex.php");
 
-use Booj\CodeTest;
+use PHPUnit\Framework\Exception;
 use PHPUnit\Framework\TestCase;
 
 class EquilibriumIndexTest extends TestCase
 {
+    /**
+     *
+     */
 	public function testComputeEquilibrium()
 	{
-		$input = [-7, 1, 5, 2, -4, 3, 0];
-		$this->assertEquals(array(3,6), Booj\CodeTest\getEquilibriums($input));
-	}
+        // Booj litmus test
+        $input = [-7, 1, 5, 2, -4, 3, 0];
+		$this->assertEquals([3,6], getEquilibriums($input));
+
+        // special case of empty array
+        $input = [];
+        $this->assertEquals([], getEquilibriums($input));
+
+        // special case of [0, N] solution
+        $input = [0, -3, -2, -1, 1, 2, 3, 0];
+        $this->assertEquals([0, 7], getEquilibriums($input));
+
+        // special case of null element
+        $input = [1, 0, -1, null];
+        $this->assertEquals([3], getEquilibriums($input));
+    }
+
+    /**
+     * @expectedException Exception
+     */
+    public function testComputeEquilibriumExceptions()
+	{
+        // special case of array in an array
+        $input = [1, 0, -1, [8, 9]];
+        getEquilibriums($input);
+
+        // special case of an array with an object
+        $input = [1, 0, -1, new \StdClass];
+        getEquilibriums($input);
+    }
 }
