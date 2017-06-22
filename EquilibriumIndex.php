522986286385dec147d51951c049d27577bdaab4
Dan Webb <dan.webb8706@gmail.com>
2017-06-22 12:35:47 -0500

    EquilibriumIndex.php
    Added logic to the function so it outputs any equilibrium indexes of an input array

--- EquilibriumIndex.php
+++ EquilibriumIndex.php
@@ -1,9 +1,45 @@
 <?php
-
-# Put a valid docblock here!
+/**
+* Name: getEquilibriums
+* 
+* Purpose: Find any equilibrium indexes in a PHP array. 
+*
+* @param array of integer(s)
+*
+* @return array of integer(s)
+*
+**/
 function getEquilibriums($arr) {
-	$output = array();
-	# Logic goes here!
-	return $output;
+  //any equilibrium index points will be added to this array	
+  $output = array();
+
+  //check if array 
+  if( !is_array($arr) )
+  		return;
+
+  //get the sum of the array (i.e. the sum of all indices when at the element furthest to the right)
+  $sum = array_sum($arr);
+
+  //starting point to compare in indices in the loop
+  $start = 0; 
+
+  //loop through the input array to begin logic to check for equilibrium indices
+  foreach($arr as $key => $value){
+            
+            //furthest right position sum minus value of the array at current index
+  			$sum = ($sum - $value);
+
+  			//if the sum of the array is in equilibrium to the left side at this index, add it to the output array
+  			if($sum == $start)
+  					array_push($output, $key);
+
+  			//the left side of the array set to its value from last iteration plus the value of the current index
+  			$start = ($start + $value);
+
+  }
+
+  return $output;
 }
+
+
  
